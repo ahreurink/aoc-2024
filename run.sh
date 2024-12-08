4e9a414c9a99a4d747a901986dad5fbed7b1f4e8
ahreurink <ahreurink@gmail.com>
2024-12-08 07:26:33 +0100

    Remove debug print for run script

--- run.sh
+++ run.sh
@@ -5,6 +5,5 @@ javac -d output day$1/Main.java
 if [ "$3" = "ex" ]; then
   cat day$1/example_input | java -cp output day$1.Main $2
 else
-  echo input
   cat day$1/input | java -cp output day$1.Main $2
 fi
