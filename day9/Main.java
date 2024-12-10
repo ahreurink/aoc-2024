68e2f3c280cbabbb2db67a51bd5138d757597e41
ahreurink <ahreurink@gmail.com>
2024-12-10 07:50:43 +0100

    Add day 9 part 2 solution

--- day9/Main.java
+++ day9/Main.java
@@ -37,33 +37,79 @@ public class Main {
     long sum = 0;
     int fileId = 0;
     int index = 0;
-    fileId = fileIds.get(index);
-    while(fileId != -1) {
-      sum += index * fileId;
-      index++;
+    for(int i = 0; i < fileIds.size(); i++) {
       fileId = fileIds.get(index);
+      if(fileIds.get(index) != -1) {
+        sum += index * fileId;
+      }
+      index++;
     }
     return sum;
   }
 
-  public static void main(String[] args) { 
-    String disk = input.nextLine();
-    List<Integer> fileIds = loadDisk(disk, new ArrayList<>());
-    int startAt = 0;
-    for(int i = fileIds.size() - 1; i >= 0; i--) {
-      int id = fileIds.get(i);
-      if(id != -1) {
-        startAt = addToFront(id, fileIds, startAt, i);
+  static int moveBlock(int position, List<Integer> fileIds, int startAt) {
+    int id = fileIds.get(position);
+    if(id != -1) {
+      startAt = addToFront(id, fileIds, startAt, position);
+    }
+    return startAt;
+  }
+
+  static int findFreeSpot(int fromIndex, List<Integer> abstractDisk, int size) {
+    for(int i = 1; i < fromIndex; i+=2) {
+      int spaceToCheck = abstractDisk.get(i);
+      if(spaceToCheck - size >= 0) {
+        abstractDisk.set(fromIndex, 0);
+        abstractDisk.set(i, spaceToCheck - size);
+        abstractDisk.set(i-1, abstractDisk.get(i-1) + size);
+        return i; 
       }
     }
-    
+    return -1;
+  }
+
+  static List<Integer> moveOnConcreteDisk(List<Integer> fileIds, List<Integer> abstractDisk, 
+                                int freeIndex, int removedIndex, int size) {
+    int freeIndexCalculated = -size; 
+    int removedIndexCalculated = 0; 
+    for(int i = 0; i < removedIndex; i++) {
+      if(i < freeIndex)
+        freeIndexCalculated += abstractDisk.get(i);
+      removedIndexCalculated += abstractDisk.get(i);
+    }
+    for(int i = 0; i < size; i++) {
+      fileIds.set(freeIndexCalculated + i, fileIds.get(removedIndexCalculated + i));
+      fileIds.set(removedIndexCalculated + i, -1);
+    }
+    return fileIds;
+  }
+
+  public static void main(String[] args) {
+    String disk = input.nextLine();
+    List<Integer> abstractDisk = new ArrayList<>();
+    for(char c : disk.toCharArray()) {
+      abstractDisk.add((int) c - '0');
+    }
+    List<Integer> originalDisk = new ArrayList(abstractDisk);
+
+    List<Integer> fileIds = loadDisk(disk, new ArrayList<>());
+    int startAt = 0;
     if(args[0].equals("1")) {
-      long checkSum = calculateCheckSum(fileIds);
-      System.out.println(fileIds);
-      System.out.println(checkSum);
+      for(int i = fileIds.size() - 1; i >= 0; i--) {
+        startAt = moveBlock(i, fileIds, startAt);
+      }
     }
     else {
-      System.out.println("Not ready for part 2");
+      for(int checkIndex = abstractDisk.size() - 1; checkIndex >= 0; checkIndex-=2) {
+        int size = originalDisk.get(checkIndex);
+        int freeIndex = findFreeSpot(checkIndex, abstractDisk, size);
+        if(freeIndex != -1) {
+          fileIds = moveOnConcreteDisk(fileIds, abstractDisk, freeIndex, checkIndex, size);
+        }
+      }
     }
+
+    long checkSum = calculateCheckSum(fileIds);
+    System.out.println(checkSum);
   }
 }
